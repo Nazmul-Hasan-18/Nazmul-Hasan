b99ee71357a78cb909b6b39b844e82b094b6d398
Nazmul Hasan <nazmulhasan.official98@gmail.com>
2025-12-28 20:34:15 +0600

    change

--- src/layout/home/skill/Skill.jsx
+++ src/layout/home/skill/Skill.jsx
@@ -49,8 +49,9 @@ const items = [
 const Skill = () => {
   return (
     <div className='bg-[#222831] py-10 lg:py-20'>
-      <div className='flex flex-col  lg:items-center gap-6 lg:gap-10'> 
-        <div className='px-6'>
+      <div className='flex flex-col  lg:items-center gap-6 lg:gap-10'>
+        <div className='px-6' data-aos="fade-up"
+          data-aos-duration="3000">
           <h2 className='lg:text-[45px] text-[32px] text-white font-semibold font-[poppins]'>My <span className='main_color'>Skill</span></h2>
         </div>
         <div className='container mx-auto px-6 lg:px-46 grid grid-cols-1 lg:grid-cols-12 gap-10'>
@@ -59,19 +60,22 @@ const Skill = () => {
           <div className='lg:col-span-8'>
             <div className='grid grid-cols-2 lg:grid-cols-3 gap-4  lg:gap-6'>
               {items.map((item, i) => (
-                <div
-                  key={i}
-                  className='bg-white/10 backdrop-blur-2xl flex flex-col items-center justify-center pt-5 px-20 rounded-xl'
-                >
-                  <div className='skill_sdow rounded-full'>
-                    <SkillCircle percent={item.rating} />
-                  </div>
+                <div data-aos="fade-up"
+                  data-aos-duration="3000">
+                  <div
+                    key={i}
+                    className='bg-white/10 backdrop-blur-2xl flex flex-col items-center justify-center pt-5 px-20 rounded-xl'
+                  >
+                    <div className='skill_sdow rounded-full'>
+                      <SkillCircle percent={item.rating} />
+                    </div>
 
-                  <div className='flex items-center gap-1 mt-4 lg:mt-6 pb-3 lg:pb-4'>
-                    <p className='text-yellow-500 text-xl lg:text-2xl'>{item.Icon}</p>
-                    <p className=' font-[Quicksand] font-semibold lg:text-lg text-gray-200'>
-                      {item.name}
-                    </p>
+                    <div className='flex items-center gap-1 mt-4 lg:mt-6 pb-3 lg:pb-4'>
+                      <p className='text-yellow-500 text-xl lg:text-2xl'>{item.Icon}</p>
+                      <p className=' font-[Quicksand] font-semibold lg:text-lg text-gray-200'>
+                        {item.name}
+                      </p>
+                    </div>
                   </div>
                 </div>
               ))}
